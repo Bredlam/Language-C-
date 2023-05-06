793e11c009f863d84069da34b70c1117d5468c01
Andrey Vavilov <st.bredlam@gmail.com>
2023-05-06 20:29:40 +0300

    добавил изменения

---
+++ IntroToProgLang/Lesson_003/Program.cs
@@ -0,0 +1,379 @@
+﻿// УРОК 3. Методы, циклы, массивы.
+Console.Clear();
+void Method1() // Войд-метод(процедура) который не принимает аргументов и ничего не возвращает ... а просто исполняет код
+{
+    Console.WriteLine("МояПодпись");
+}
+Method1(); // Вызов метода
+
+void Method2(string msg) // Войд-метод(процедура) который принимает аргументы, но ничего не возвращает ... а просто исполняет код
+{
+    Console.WriteLine(msg);
+}
+Method2("Текст сообщения"); // Вызов метода с указанием литерала в качестве аргумента
+Method2(msg: "Именнованый аргумент"); // Вызов метода с указанием именнованного аргумента
+
+int Method3() // Метод который не принимает аргументы, но возвращает (return) результат своего исполнения
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();   // Вызов метода ... раз он возвращает результат исполнения то значит должна быть переменная куда он запишет
+                        // возвращаемое значение
+Console.WriteLine(year);
+
+string Method4(int count, string text01) // Метод который принимает аргументы и возвращает () результат своего исполнения
+{
+    int i = 0;
+    string result01 = String.Empty; // Положили в переменную пустое значение строкового типа
+
+    while (i < count)
+    {
+        result01 = result01 + text01;
+        i++;
+    }
+    return result01;
+}
+string result02 = Method4(10, "+"); // Вызов метода ... раз он возвращает результат исполнения то значит должна быть переменная 
+                                    // куда он запишет возвращаемое значение
+Console.WriteLine(result02);
+
+string Method5(int count, string text02) // Метод который принимает аргументы и возвращает () результат своего исполнения
+{
+    string result01 = String.Empty; // Положили в переменную пустое значение строкового типа
+
+    for (int i = 0; i < count; i++) // Первый раз использую цикл for ...
+    {
+        result01 = result01 + text02;
+    }
+    return result01;
+}
+string result03 = Method5(10, "0"); // Вызов метода ... раз он возвращает результат исполнения то значит должна быть переменная 
+                                    // куда он запишет возвращаемое значение
+Console.WriteLine(result03);
+
+// Задача. Вывести таблицу умножения используя цикл
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+
+// Задача. Дан текст, в тексте нужно все пробелы заменить черточками, маленькие буквы "к" заменить большими "К"
+// а большие "С" заменить маленькими "с".
+string oldText = "дан текст нужно пробелы заменить на черточки";
+string Replase(string text, char oldValue, char newValue)
+{
+    string result = String.Empty; // Заводим пустую строку
+    int length = text.Length; // Получаем количество символов в строке
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result += $"{newValue}";
+        else result += $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replase(oldText, ' ', '-');
+Console.WriteLine(newText);
+
+// Задача. Создать массив, заполнить рандомно цифрами, упорядочить от минимального к максимальному
+int[] array = { 1, 5, 4, 3, 2, 6, 7, 1, 1 }; // Создали массив и заполнили значениями
+void PrintArray(int[] array) // Метод вывода на экран элементов массива
+{
+    int count = array.Length; // Получили длину массива
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(array[i] + ",");
+    }
+    Console.WriteLine();
+}
+PrintArray(array);
+void SelectionSortMinMax(int[] array) // Метод сортировки массива от меньшего к большему
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int minPosition = i; // Передали в переменную индекс позиции
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int tempPosition = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = tempPosition;
+    }
+}
+SelectionSortMinMax(array);
+PrintArray(array);
+// ПодЗадача. Создать массив, заполнить рандомно цифрами, упорядочить от максимального к минимальному
+void SelectionSortMaxMin(int[] array) // Метод сортировки массива от меньшего к большему
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int maxPosition = i; // Передали в переменную индекс позиции
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+        int tempPosition = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = tempPosition;
+    }
+}
+SelectionSortMaxMin(array);
+PrintArray(array);
+
+// Задача. Напишите программу которая принимает на вход координаты точки (Х и У), причем Х ≠ 0 и У ≠ 0 и выдает номер
+// четверти плоскости, в которой находится эта точка
+void UserInputCoord(ref int X, ref int Y) // Определение метода с передачей параметров по ссылке (ref)
+{
+    while (true)
+    {
+        try
+        {
+            Console.Write("Введите Х: ");
+            X = int.Parse(Console.ReadLine() ?? "");
+            break;
+        }
+        catch (Exception exc)
+        {
+            Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        }
+    }
+    while (true)
+    {
+        try
+        {
+            Console.Write("Введите Y: ");
+            Y = int.Parse(Console.ReadLine() ?? "");
+            break;
+        }
+        catch (Exception exc)
+        {
+            Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        }
+    }
+}
+int X = 0, Y = 0;
+UserInputCoord(ref X, ref Y); // Вызов метода
+if (X > 0 && Y > 0)
+    Console.WriteLine("1");
+else if (X < 0 && Y > 0)
+    Console.WriteLine("2");
+else if (X < 0 && Y < 0)
+    Console.WriteLine("3");
+else if (X > 0 && Y < 0)
+    Console.WriteLine("4");
+else
+    Console.WriteLine("Ошибка! Точка попадает на оси координат!");
+
+// Задача. Напиши программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти
+// (Х и У). Например, если четверть - 1 то Х > 0 и У > 0
+int NQ = 0;
+UnputUserNumberOfQuarter(ref NQ); // Вызов метода
+FindQuarter(NQ); // Вызов метода
+void UnputUserNumberOfQuarter(ref int NQ) // Определение метода
+{
+    while (true)
+    {
+        try
+        {
+            Console.Write("Введите номер четверти: ");
+            NQ = int.Parse(Console.ReadLine() ?? "");
+            break;
+        }
+        catch (System.Exception exc)
+        {
+            Console.WriteLine($"Ошибка! {exc.Message}"); ;
+        }
+    }
+}
+void FindQuarter(int NQ) // Определение метода
+{
+    if (NQ == 1) Console.Write("Диапазон чисел Х > 0 и У > 0");
+    else if (NQ == 2) Console.Write("Диапазон числе Х < 0 и У > 0");
+    else if (NQ == 3) Console.Write("Диапазон чисел Х < 0 и У < 0");
+    else if (NQ == 4) Console.Write("Диапазон чисел Х > 0 и У < 0");
+    else
+    {
+        Console.WriteLine("Вы можете ввести цифру в диапазоне 1 ... 4");
+    }
+}
+
+// Задача. Напишите программу, которая принимает на вход координаты двух точек и находит
+// расстояние между ними в 2D пространстве.
+// А(3, 6); B (2, 1) -> 5,09
+// А(7,-5); B (1,-1) -> 7,21 
+double[] Coordinates = { 0, 0, 0, 0 };
+string XY = string.Empty;
+UserControlInput01(ref Coordinates, XY); // Вызов метода
+void UserControlInput01(ref double[] Coordinates, string XY) // Определение метода
+{
+    for (int i = 0; i < Coordinates.Length; i++)
+    {
+        int g = 1 + i;
+        if ((g + 1) % 2 == 0)
+            XY = "X";
+        else
+            XY = "Y";
+
+        Console.Write($"Введите координату {XY} ");
+
+        while (true)
+        {
+            try
+            {
+                Coordinates[i] = double.Parse(Console.ReadLine() ?? "");
+                break;
+            }
+            catch (System.Exception exc01)
+            {
+                Console.WriteLine($"Ошибка! Введите число {exc01.Message}");
+            }
+        }
+    }
+}
+double SecondPower = 2;
+double Result01 = Math.Pow(Coordinates[0] - Coordinates[2], SecondPower);
+double Result02 = Math.Pow(Coordinates[1] - Coordinates[3], SecondPower);
+double Result03 = Math.Sqrt(Result01 + Result02);
+Console.Write($"Итог {Result03}");
+
+// Задача. Напишите программу, которая принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N
+// Например, 5 -> 1, 4, 9, 16, 25
+int UserNum01 = 0;
+ControlUserNum(ref UserNum01); // Вызов метода
+UserCalculations01(UserNum01); // Вызов метода
+void ControlUserNum(ref int Num) // Определение метода
+{
+    while (true)
+    {
+        try
+        {
+            Console.Write("Введите число: ");
+            Num = int.Parse(Console.ReadLine() ?? "");
+            break;
+        }
+        catch (System.Exception exc02)
+        {
+            Console.WriteLine($"Ошибка! Введите число {exc02}");
+        }
+    }
+}
+void UserCalculations01(int Num) // Определение метода
+{
+    int st = 2;
+    for (int i = 1; i <= Num; i++)
+    {
+        int Result04 = Convert.ToInt32(Math.Pow(i, st));
+        Console.Write($"{Result04}, ");
+    }
+}
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
+// Например: А(3, 6, 8) / В(2, 1, -7) -> 15,84
+Console.Clear();
+string[] nameCoordinates = { "x1", "y1", "z1", "x2", "y2", "z2" };
+double[] coordinates = { 0, 0, 0, 0, 0, 0 };
+
+UserControlInput02(ref coordinates, nameCoordinates); // Вызов метода
+CalculatingLengthVector(coordinates); // Вызов метода
+
+void UserControlInput02(ref double[] coord, string[] nameCoord) // Определение метода - контроль ввода данных        
+{
+    for (int i = 0; i < coord.Length; i++)
+    {
+        while (true)
+        {
+            try
+            {
+                Console.Write($"Введите координаты {nameCoord[i]}: ");
+                coord[i] = double.Parse(Console.ReadLine() ?? "");
+                break;
+            }
+            catch (System.Exception exc)
+            {
+                Console.WriteLine($"Ошибка! Введите число {exc.Message}");
+            }
+        }
+    }
+
+}
+void CalculatingLengthVector(double[] coord) // Определение метода - вычисления расстояния между 2-х точек в 3D пространсте
+{
+    double result01 = 0;
+    for (int i = 0; i < coord.Length - 3; i++)
+    {
+        double result02 = Math.Pow(coord[i] - coord[i + 3], 2);
+        result01 += result02;
+    }
+    Console.Write($"{(Math.Sqrt(result01)):f2}");
+}
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+// Примеры: 14212 -> нет ... 23432 -> да
+Console.Clear();
+
+int userNum02 = UserControlInput03(); // Вызов метода
+CalculationPalindrome(userNum02) // Вызов метода
+;
+static int UserControlInput03() // Определение метода - контроль ввода пятизначного числа      
+{
+    while (true)
+    {
+        Console.Write("Введите целое пятизначное число: ");
+        if ((int.TryParse(Console.ReadLine(), out int userInt)))
+        {
+            int controlInt = 0;
+            int coint = userInt;
+            while (coint > 0)
+            {
+                controlInt++;
+                coint /= 10;
+            }
+            if (controlInt == 5)
+            {
+                return userInt;
+            }
+        }
+        Console.WriteLine("Ошибка. Введите целое пятизначное число!");
+    }
+}
+static void CalculationPalindrome(int num) // Определение метода - вычисление палиандрома
+{
+    if (num / 10000 == num % 10 && num / 1000 % 10 == num / 10 % 10)
+        Console.Write($"{num} -> Да");
+    else
+        Console.Write($"{num} -> Нет");
+}
+
+// Напишите программу, которая принимает на вход число (N) и выдает таблицу кубов чисел от 1 до N
+// Например: 5 -> 1, 8, 27, 64, 125
+int userNum03 = ControlUserInput(); // Вызов метода
+UserCalculations02(userNum03);
+
+static int ControlUserInput() // Определение метода - 
+{
+    while (true)
+    {
+        try
+        {
+            Console.Write("Введите число: ");
+            int num = int.Parse(Console.ReadLine() ?? "");
+            return num;
+        }
+        catch (System.Exception exc)
+        {
+            Console.WriteLine($"Ошибка! Введите число: {exc.Message}");
+        }
+    }
+}
+static void UserCalculations02(int num) // Определение метода - 
+{
+    for (int i = 1; i <= num; i++)
+    {
+        int result = Convert.ToInt32(Math.Pow(i, 3));
+        Console.Write($"{result}, ");
+    }
+}
--- IntroToProgLang/Lesson_005/Program.cs
+++ IntroToProgLang/Lesson_005/Program.cs
@@ -1,2 +1,267 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+#region Задача 
+
+    #region Degining Methods
+    
+    #endregion    
+#endregion
+
+// УРОК 5. ЛЕКЦИЯ
+// На уроке была теория о том как правильно писать код с точки зрения чистоты кода и общепринятых правил при взаимодействии
+// программистов. Суть - код читается чаще чем пишется поэтому писать нужно так чтобы читать было легко.
+
+// УРОК 5. СЕМИНАР
+#region Задача 31 Преподаватель разобрал задачу
+// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9].
+// Найдите сумму отрицательных и положительных элементов массива.
+// По этой задаче преподаватель пытался донести мысль, что нужно стараться инструкции выводить в методы т.к.
+// работа с методами дает больше возможстей при дальнейшем изменении кода по сравнению с тем если нам
+// придется работать с некими командами привязанными например к определенным классам напрямую. В качестве примера
+// я создал два метода и перенес туда инструкции печати массива и вычислений из массива
+
+int[] array01 = GetArray01(12, -9, 9);
+PrintArray01(array01);
+int positiveSum = GetPositiveSum(array01);
+int negativeSum = GetNegativeSum(array01);
+PrintNegativeAndPositiveSumOfArray(positiveSum, negativeSum);
+
+    #region Defining Methods
+    // Метод возвращает массив заполенный случайными числами
+    int[] GetArray01(int size, int minValue, int maxValue)
+    {
+        int[] res = new int[size]; // Cоздаем массив и размер массива определяем входящим аргументом
+
+        for (int i = 0; i < size; i++)
+        {
+            res[i] = new Random().Next(minValue, maxValue + 1); // Класс Random() и его метод .Next
+        }
+        return res;
+    }
+
+    // Метод вычисляет сумму положительных чисел в массиве и возвращает получившийся результат
+    int GetPositiveSum(int[] array)
+    {
+        int positiveSum = 0;
+        foreach (int element in array)  // foreach (пер."для каждого") - удобно использовать этот цикл когда нет нужды 
+                                        // привязываться к индексам, но нужно перебрать все элементы коллекции. Также 
+                                        // этот цикл работает только на считывание т.е. не позволяет изменить коллекцию.
+        {
+            if (element > 0) positiveSum += element;
+        }
+        return positiveSum;
+
+    }
+
+    // Метод вычисляет сумму отрицательных чисел в массиве и возвращает получившийся результат
+    int GetNegativeSum(int[] array)
+    {
+        int negativeSum = 0;
+        foreach (int element in array)
+        {
+            negativeSum += element < 0 ? element : 0;   // тут мы используем тернарный оператор, если element < 0 то            
+                                                        // вернется ИСТИНА т.е. element
+                                                        // иначе вернется ЛОЖЬ т.е. 0
+        }
+        return negativeSum;
+    }
+
+    // Метод выводит на печать массив. По рекомендации преподавателя иструкция была обернута в метод
+    void PrintArray01(int[] array)
+    {
+        Console.WriteLine(String.Join(" ", array)); // Используя метод .Join из класса String мы можем вывести значения массива передав
+                                                    // массив в метод в качестве второго аргумента, а первым аргументом у нас выступает
+                                                    // разделитель " " т.е. пробел в нашем случае
+    }
+
+    // Метод выводит на печать результат вычислений. По рекомендации преподавателя инструция была обернута в метод
+    void PrintNegativeAndPositiveSumOfArray(int positiveSum, int negativeSum)
+    {
+        Console.WriteLine($"Positive sum = {positiveSum}, Negative sum = {negativeSum} ");
+        Console.WriteLine("Задача 31. Разобрали с преподавателем");
+        Console.WriteLine();
+    }
+    #endregion
+#endregion
+
+#region Задача 32
+// Напишите программу замены элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот
+// Например: [-4,-8,8,2] -> [4,8,-8,-2]
+int[] array02 = {-6,-8,8,2};
+int[] array03 = ReversingPolarityOfNumbersInArray(array02);
+PrintArray02(array03);
+
+    #region Degining Methods
+        int [] ReversingPolarityOfNumbersInArray(int [] array)
+        {            
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] *= -1;
+            }
+            return array;
+        }
+
+        void PrintArray02(int[] array)
+        {
+            Console.WriteLine("Перевернутый массив");
+            Console.WriteLine(String.Join(",", array));
+            Console.WriteLine("Задача 32.");
+            Console.WriteLine();
+        }            
+    #endregion
+#endregion
+
+#region Задача 33
+// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// Например: 4; массив [6,7,19,345,3] -> нет
+// Мои добавления/уточнения к задаче:
+// - проверяемое число задает пользователь
+// - массив генерируется рандомно
+// У меня получился какой-то лотерейный автомат - повезет ли или не повезет.
+int userNum = UserInputNumber();
+int[] array04 = GenerateRandomArray01();
+bool result01 = FoundNumberInArray(userNum, array04);
+PrintResult01(result01, array04);
+
+    #region Degining Methods
+        int UserInputNumber()
+        {
+            while (true)
+            {
+                Console.Write("Введите число которое надо найти в массиве: ");
+                if(int.TryParse(Console.ReadLine(), out int userUnput)) return userUnput;
+            }
+        }
+        int[] GenerateRandomArray01()
+        {
+            var tempRandom = new Random();
+            int[] array = new int[tempRandom.Next(5,11)]; // Создали массив с рандомной длиной в диапазоне [5,11)
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = tempRandom.Next(1,100);
+            }
+            return array;
+        }
+        bool FoundNumberInArray(int num, int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(array[i] == num) return true;                
+            }
+            return false;
+        }
+        void PrintResult01(bool result, int[] array)
+        {
+            Console.WriteLine($"[{String.Join(",", array)}] -> {result}");
+            Console.WriteLine("Задача 33.");
+            Console.WriteLine();
+        }            
+    #endregion
+#endregion
+
+#region Задача 35
+// Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке
+// [10,99]. Например: [5,18,123,6,2] -> 1 
+int[] array05 = GenerateRandomArray02();
+int result02 = FoundRangeOfNumberInArray(array05);
+PrintResult02(result02, array05);
+
+    #region Degining Methods
+        int [] GenerateRandomArray02()
+        {
+            int[] array = new int[10]; // По задаче длина массива 123, но я поставил поменьше чтобы можно было посмотреть содержимое
+                                        // массива
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(1,1000);
+            }
+            return array;
+        }
+        int FoundRangeOfNumberInArray(int[] array)
+        {
+            int collector = 0;
+            foreach (int el in array)
+            {
+                collector += el > 9 && el < 100 ? 1 : 0;
+            }
+            return collector;
+        }
+        void PrintResult02(int result, int[] array)
+        {
+            Console.WriteLine($"Массив: [{String.Join(",", array)}] Количество элементов в массиве -> {result}");
+            Console.WriteLine("Задача 35.");
+            Console.WriteLine();
+        }
+    #endregion
+#endregion
+
+#region Задача 37
+// Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+// Результат запишите в новом массиве. Например: [1,2,3,4,5] -> [5,8,3]
+int[] array06 = {1,2,3,4,5,6,7};
+int[] array07 = ProductOfArrayNumbers(array06);
+PrintArray03(array07);
+
+    #region Degining Methods
+    int[] ProductOfArrayNumbers(int[] array)
+    {
+        int lehgthArray = array.Length % 2 == 0 ? array.Length / 2 : (array.Length + 1) / 2;
+        int[] arrayResult = new int[lehgthArray];
+        if(array.Length % 2 == 0)
+            for (int i = 0; i < lehgthArray; i++)
+            {
+                arrayResult[i] = array[i] * (array[array.Length-1] - i);
+            }
+        else
+        {
+            for (int i = 0; i < lehgthArray-1; i++)
+            {
+                arrayResult[i] = array[i] * (array[array.Length-1] - i);
+            }
+            arrayResult[lehgthArray-1] = array[lehgthArray-1];
+        }
+        return arrayResult;
+    }
+    void PrintArray03(int[] array)
+    {
+        Console.WriteLine($"{String.Join(",", array)}");
+        Console.WriteLine("Задача 37.");
+        Console.WriteLine();
+    }
+            
+    #endregion
+#endregion
+
+#region Домашняя задача 34
+// Задайте массив заполненный случайными положительными трехзначными числами. Напишите программу, которая покажет количество четных
+// чисел в массиве. Например: [345, 897, 568, 234] -> 2
+
+    #region Degining Methods
+        
+    #endregion
+#endregion
+
+#region Домашняя задача 36
+// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечетных позициях.
+// Например: [3,7,23,12] -> 19
+    
+    #region Degining Methods
+        
+    #endregion
+#endregion
+
+#region Домашняя задача 38
+// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// Например: [3,7,22,2,78] -> 76
+    
+    #region Degining Methods
+        
+    #endregion
+#endregion
+
+#region Задача 
+
+    #region Degining Methods
+    
+    #endregion    
+#endregion
